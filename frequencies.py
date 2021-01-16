b791c65e8f9fca0a52580bf6e413615495ea57e0
deusxaviermachina <phenehan0@gmail.com>
2021-01-16 16:16:32 -0500

    initial commit

---
+++ frequencies.py
@@ -0,0 +1,12 @@
+import pprint
+import re
+
+stack = []
+with open("files/newfile.txt", "rt") as fh:
+    for i in fh:
+        if i != "\n" and re.match(r"^http://wikipedia\.org//wiki/[\wA-z]+$", i):
+            stack.append(i.strip("\n"))
+dict = {i: stack.count(i) for i in l}
+data = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+pprint.pprint(data, width=1)
+print(len(stack))
