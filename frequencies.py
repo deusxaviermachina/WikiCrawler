aa8673d85a232258d7829b378e231b909a789a9d
phenehan <58574036+deusxaviermachina@users.noreply.github.com>
2021-01-16 16:19:36 -0500

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,7 +4,7 @@ import re
 stack = []
 with open("files/newfile.txt", "rt") as fh:
     for i in fh:
-        if i != "\n" and re.match(r"^http://wikipedia\.org//wiki/[\wA-z]+$", i):
+        if i != "\n" and re.match(r"^http://wikipedia\.org//wiki/[\w0-9]+$", i):
             stack.append(i.strip("\n"))
 dict = {i: stack.count(i) for i in l}
 data = sorted(dict.items(), key=lambda x: x[1], reverse=True)
