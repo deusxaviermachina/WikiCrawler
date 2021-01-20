4212a26a7e2771b8424bd3f3422fbe4e973f79a3
deusxaviermachina <phenehan0@gmail.com>
2021-01-20 00:46:56 -0500

    updated

--- frequencies.py
+++ frequencies.py
@@ -2,22 +2,24 @@ import pprint
 import re
 import os
 
-<<<<<<< HEAD
 """
-running 'wikiscraper.main' yields a directory containing
-a .txt file for each sub-tree. The filenames correspond to the root
-nodes of each sub-tree. So, each tree is a directory.
-Each sub-tree is a within that directory. Each filename is a root node.
-The urls contained within each file are branches of a sub-tree. 
+running 'wikiscraper.main' yields a [root node].txt file for each sub-tree.
+So, each tree is a directory. Each sub-tree is a file within that directory.
+The urls contained within each [root node] file are branches (which may themselves be
+roots to other sub-trees)
 """
+
 tree = os.listdir("files")
 
 nodes= []
 for subtree_root in tree:
+    root_name = subtree_root.replace("httpwikipediaorgwiki", "").replace(".txt", "")
     with open(f"files/{subtree_root}", "rt") as fh:
         for subtree_branch in fh:
             if subtree_branch != "\n" and re.match(r"^http://wikipedia\.org//wiki/[\w0-9]+$",subtree_branch):
-                nodes.append(subtree_branch.strip("\n"))
+                nodes.append(subtree_branch.split("wiki/")[1])
+                nodes.append(root_name)
+
 
 """
 map nodes to frequencies and sort in ascending order of frequencies--strongly connected 
@@ -28,14 +30,3 @@ a semantic connection between the subjects of the articles
 data = {node.strip("\n"): nodes.count(node) for node in nodes}
 data = sorted(data.items(), key=lambda x:x[1], reverse=True)
 pprint.pprint(data)
-=======
-stack = []
-with open("files/newfile.txt", "rt") as fh:
-    for i in fh:
-        if i != "\n" and re.match(r"^http://wikipedia\.org//wiki/[\w0-9]+$", i):
-            stack.append(i.strip("\n"))
-dict = {i: stack.count(i) for i in l}
-data = sorted(dict.items(), key=lambda x: x[1], reverse=True)
-pprint.pprint(data, width=1)
-print(len(stack))
->>>>>>> b789532c21a449dec41a91b10bc4beddc938bf7c
